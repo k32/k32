3eebbae87c5295c8c68c178768b701f0ade2a214
k32 <10274441+k32@users.noreply.github.com>
2020-11-09 01:17:16 +0100

    Make some progress on the post about troubleshooting

--- publish.el
+++ publish.el
@@ -6,6 +6,7 @@
 (require 'webfeeder)
 (require 'font-lock)
 (require 'erlang)
+(require 'dash)
 
 (setq make-backup-files nil)
 
@@ -28,34 +29,43 @@
 (setq htmlize-use-rgb-map 'force)
 (setq out-dir "./out")
 
+(setq my-org-publish-props
+      '(:base-extension "org"
+        :publishing-function org-html-publish-to-html
+        :html-validation-link nil
+        :auto-sitemap nil
+        :sitemap-title "k32's notebook"
+        :sitemap-filename "index.org"
+        :sitemap-sort-files chronologically
+        :html-link-home "index.html"
+        :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"default.css\"/>
+<link href=\"atom.xml\" type=\"application/atom+xml\" rel=\"alternate\" title=\"k32's notebook\"/>"
+        :html-head-include-default-style nil
+        :html-head-extra nil
+        :html-preamble nil
+        :html-head-include-scripts nil
+        ;; :html-postamble nil
+        :html-htmlize-output-type css
+        :with-toc nil
+        :with-author nil
+        :with-email nil
+        :time-stamp-file nil
+        :with-section-numbers nil))
+
 (setq org-publish-project-alist
-      `(("posts"
-         :base-directory "./posts"
-         :base-extension "org"
-         :publishing-function org-html-publish-to-html
-         :publishing-directory ,out-dir
-         :html-validation-link nil
-         :auto-sitemap nil
-         :sitemap-title "k32's notebook"
-         :sitemap-filename "index.org"
-         :sitemap-sort-files chronologically
-         :html-link-home "index.html"
-         :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"default.css\"/>"
-         :html-head-include-default-style nil
-         :html-head-extra nil
-         :html-preamble nil
-         :html-head-include-scripts nil
-         :html-postamble nil
-         :html-htmlize-output-type css
-         :with-toc nil
-         :with-author nil
-         :with-email nil
-         :with-section-numbers nil)
+      `(,(-concat `("posts"
+                    :base-directory "./posts"
+                    :publishing-directory ,out-dir)
+                  my-org-publish-props)
+        ,(-concat `("drafts"
+                    :base-directory "./drafts"
+                    :publishing-directory "./drafts-out")
+                  my-org-publish-props)
         ("static"
          :publishing-function org-publish-attachment
          :publishing-directory "./out"
          :base-directory "./static"
-         :base-extension "css")
+         :base-extension "css\\|ico")
         ("blog"
          :components ("posts" "static"))))
 
